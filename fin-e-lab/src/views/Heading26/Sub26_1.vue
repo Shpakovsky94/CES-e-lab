2314064ff32cb6395f886f595f3b7a14bc5d7419
Oleks Oleks <Shpakovsky94@gmail.com>
2020-12-02 16:19:12 +0200

    all headings are setted up

--- fin-e-lab/src/views/Heading26/Sub26_1.vue
+++ fin-e-lab/src/views/Heading26/Sub26_1.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <iframe
+        <iframe
+      src="https://docs.google.com/spreadsheets/d/1Jc2xh-YhwLnXCOT2xdXYhcj9welO966_/edit"
+      width="100%"
+      height="850px"
+      frameborder="0"
+      >
+      </iframe>
+      
+    <!-- <iframe
       src="https://consolis-my.sharepoint.com/personal/mihkel_jaaniste_consolis_com/_layouts/15/Doc.aspx?sourcedoc={4b888def-3dcb-44ed-b0fa-b01c5a29136c}&amp;action=embedview&amp;wdPrint=0&amp;wdEmbedCode=0"
       width="100%"
       height="850px"
@@ -8,7 +16,7 @@
       >This is an embedded
       <a target="_blank" href="https://office.com">Microsoft Office</a>
       document, powered by
-      <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+      <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe> -->
   </div>
 </template>
 
--- fin-e-lab/src/views/Heading26/Sub26_2.vue
+++ fin-e-lab/src/views/Heading26/Sub26_2.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <iframe
+            <iframe
+      src="https://docs.google.com/spreadsheets/d/19atsD_bT_Q9onmR9daW-FkownzesPDGy/edit"
+      width="100%"
+      height="850px"
+      frameborder="0"
+      >
+      </iframe>
+
+    <!-- <iframe
       src="https://consolis-my.sharepoint.com/personal/mihkel_jaaniste_consolis_com/_layouts/15/Doc.aspx?sourcedoc={e2f644d6-92f0-4046-8b6b-7df9ee485e13}&amp;action=embedview&amp;wdPrint=0&amp;wdEmbedCode=0"
       width="100%"
       height="850px"
@@ -8,7 +16,7 @@
       >This is an embedded
       <a target="_blank" href="https://office.com">Microsoft Office</a>
       document, powered by
-      <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+      <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe> -->
   </div>
 </template>
 
